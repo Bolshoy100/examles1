8d9bb483f5436af9a2aae67b87933b8e36098b78
Vladimir <vl.ul.le@yandex.ru>
2022-06-30 14:57:08 +0400

    added ex5

---
+++ ex005/Program.cs
@@ -0,0 +1,10 @@
+﻿string username = Console.ReadLine();
+if (username.ToLower() == "маша")
+{
+    Console.WriteLine("Охренеть, это эе опять Маха");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
