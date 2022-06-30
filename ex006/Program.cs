956d7e838285bf93988214ff46b3247d10e1d3a9
Vladimir <vl.ul.le@yandex.ru>
2022-06-30 15:31:40 +0400

    added ex006

---
+++ ex006/Program.cs
@@ -0,0 +1,13 @@
+﻿int a = 3;
+int b = 7;
+int c = 1;
+int d = 9;
+int e = 4;
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+Console.Write("Max =  ");
+Console.WriteLine(max);
